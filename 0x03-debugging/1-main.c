81e7f4c4e0b78d5254e538144516bb9f34f40489
khaoula <khawlasandi95@gmail.com>
2023-06-22 06:54:37 -0700

    My commit

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,15 +1,11 @@
 #include <stdio.h>
 
 /**
-
 * main - causes an infinite loop
-
 * Return: 0
-
 */
 
 int main(void)
-
 {
 
 int i;
@@ -29,5 +25,4 @@ i = 0;
 printf("Infinite loop avoided! \\o/\n");
 
 return (0);
-
 }
